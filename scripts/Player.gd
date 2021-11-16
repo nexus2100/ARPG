25ea6762207dec9796812dfbf52cfb8f35e44ed1
Maksim Veremeychik <max1993dub@gmail.com>
2021-11-16 21:59:56 +0300

    kopipast is addona fpscontoler, vrode rabotaet

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody
-var gravity = Vector3(0,-9.5,0)
+var GRAVITY = Vector3(0,-9.5,0)
+var gravity = GRAVITY
 var velocity = Vector3()
 var movement = Vector3()
 var direction = Vector3()
-var forward_speed = 15
-var rlb_speed = 5
-var accel = 2
-var deaccel = 5
+var walk_speed = 8
+var acceleration = 10
+var air_acceleration = acceleration/2
+var current_acceleration = air_acceleration
 
 onready var mc = $Move_control
 onready var rc = $Rotate_control
@@ -14,6 +15,7 @@ onready var bj = $Button_jump
 onready var bs = $Button_sit
 var is_sit:bool = false
 var jump:bool = false
+var grounded:bool = false
 func _ready():
 	pass
 
@@ -24,36 +26,43 @@ func _physics_process(delta):
 
 func move_and_rotate(delta):
 	direction = Vector3.ZERO
-	var speed = 0
-	var acceleration = deaccel
 	if rc.used:
 		$Head.transform.basis = Basis(Vector3.RIGHT,clamp($Head.rotation.x - rc.vec.y*delta*rc.strength,-1.4,1.4))
 		transform.basis = Basis(Vector3.UP,rotation.y + rc.vec.x*delta*rc.strength)
-	if  abs(mc.vec.y)>0.6 and mc.strength > 0.1 and is_on_floor():
-		acceleration = accel
+	if  abs(mc.vec.y)>0.7 and mc.strength > 0.1:
 		direction.z += sign(mc.vec.y)
-		if mc.vec.y > 0: speed = forward_speed*mc.strength
-		else: speed = rlb_speed*mc.strength
-	elif  abs(mc.vec.x) > 0.6 and mc.strength > 0.1 and is_on_floor():
-		acceleration = accel
+	elif  abs(mc.vec.x) > 0.7 and mc.strength > 0.1:
 		direction.x += sign(mc.vec.x)
-		speed = rlb_speed*mc.strength
-	direction =  direction.normalized()
 	direction = direction.rotated(Vector3.UP,rotation.y)
-	velocity = velocity.linear_interpolate(direction*speed, delta*acceleration)
-	movement.x = velocity.x
-	movement.z = velocity.z
-	if !is_on_floor(): movement.y += -9.5*delta
+	if is_on_floor():
+		current_acceleration = acceleration
+		movement.y = 0
+		gravity  = -get_floor_normal()*10
+		grounded = true
+	else:
+		current_acceleration =  air_acceleration
+		if grounded:
+			gravity = Vector3()
+			grounded = false
+		else:
+			gravity += delta * GRAVITY
+	if is_on_ceiling() and gravity.y >= 0:
+		gravity.y = 0
 	if bj.pressed and is_on_floor() and !is_sit:
 		movement.y += 300*delta
-	movement = move_and_slide(movement, Vector3.UP, 0.05, 4, deg2rad(40))
-	print(movement)
 	if !bs.pressed and is_sit:
 		is_sit = false
 		$AnimationPlayer.play_backwards("sit")
 	if bs.pressed and !is_sit:
 		is_sit = true
 		$AnimationPlayer.play("sit")
+	velocity = velocity.linear_interpolate(direction * walk_speed*mc.strength, current_acceleration * delta)
+	movement.x = velocity.x + gravity.x
+	movement.z = velocity.z + gravity.z
+	movement.y = gravity.y
+	movement = move_and_slide(movement, Vector3.UP, false, 4, 0.785398, false)
+	print(movement)
+	
 
 
 
